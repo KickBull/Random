b863eb7a1c2826288e0c85ccd638badb95b0cfd5
KickBull <54203437+KickBull@users.noreply.github.com>
2022-05-31 21:38:45 +0300

    Update spacify.py

--- spacify.py
+++ spacify.py
@@ -21,7 +21,7 @@ def spacify(input_equation: str) -> str:
 
         if len(buffer) > 1:
             if buffer[1] in operators:
-                if buffer[1] in ("+", "-") and not output[-1].isdigit() and not output[-1] == ")" or buffer[1] == "^":
+                if buffer[1] in ("+", "-") and not output[-1].isdigit() and output[-1] != ")" or buffer[1] == "^":
                     output += buffer[1]
                 else:
                     output += f" {buffer[1]} "
@@ -33,3 +33,5 @@ def spacify(input_equation: str) -> str:
             output = buffer[0]
 
     return output
+
+print(spacify("4- (3 /2)*-16^ 12 -1*(+13)"))
